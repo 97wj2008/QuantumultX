3178d3afdb8ca4cce06de58b88279596d0b042c8
97wj2008 <70876921+97wj2008@users.noreply.github.com>
2022-10-24 01:44:39 +0800

    Update wps.js

--- script/wps.js
+++ script/wps.js
@@ -5,7 +5,7 @@ WPS Office 解锁部分功能
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/[a-z-]*account\.wps\.c(n|om)(:\d+|)\/api\/users url script-response-body https://raw.githubusercontent.com/97wj2008/QuantumultX/main/script/wps.jss
+^https?:\/\/[a-z-]*account\.wps\.c(n|om)(:\d+|)\/api\/users url script-response-body https://raw.githubusercontent.com/97wj2008/QuantumultX/main/script/wps.js
 
 [mitm]
 hostname = *account.wps.cn, *account.wps.com
